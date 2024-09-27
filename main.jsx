f6f3784914a87a9fa998975b026d6f7e71bc1d57
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-27 05:39:09 +0200

    exemple d utilisation - code a glisser dans l autre projet où inserer la modale avec la cde " npm link my-react-modal "

--- main.jsx
+++ main.jsx
@@ -1,1 +1,29 @@
 import './src/Modal.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Modal from './src/Modal';
+
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <h1>Hello, My React Modal</h1>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        <h2>This is the modal content!</h2>
+      </Modal>
+    </div>
+  );
+};
+
+// Render the App component into the #app div
+ReactDOM.render(<App />, document.getElementById('app'));
